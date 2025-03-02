1229e19e25c6f8d285638329cff91495a9f7a2c9
Vegetableglass <kaiosouto10@gmail.com>
2025-03-01 23:41:44 -0300

    ✨ feat- adicionando banco de dados externo e atualizando o requirements

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -75,11 +75,15 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+          'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crudDjango',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  
+        'PORT': '3306',      
     }
-}
+} 
 
 
 # Password validation
